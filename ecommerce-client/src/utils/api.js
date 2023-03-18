7085d4dc376005b2ce693ab72a476399b86c5114
ariel <ariel3332@gmail.com>
2023-03-18 23:43:46 +0200

    Adding api call to the home page

--- ecommerce-client/src/utils/api.js
+++ ecommerce-client/src/utils/api.js
@@ -1,7 +1,14 @@
 import {PROCUCTS} from "./mocks";
+import axios from 'axios';
+
+export const apiUrl = 'http://localhost:3001/api'
 
 export const getProductData = (
     productId
 ) => {
     return PROCUCTS.find((product) => product.id === productId);
 };
+
+export const getProducts = () => {
+   return axios.get(`${apiUrl}/product/`)
+}
