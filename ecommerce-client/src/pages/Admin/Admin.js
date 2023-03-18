b562dff8ef47e8593eab835b6bf48506482d6828
ariel <ariel3332@gmail.com>
2023-03-18 23:13:58 +0200

    Changing graph from google to d3

--- ecommerce-client/src/pages/Admin/Admin.js
+++ ecommerce-client/src/pages/Admin/Admin.js
@@ -1,30 +1,32 @@
 import styles from './Admin.module.css'
-import { Chart } from 'react-google-charts';
-
+import { Calendar } from './Calendar';
 const Admin = () => {
     const currnetUsersCount = 10;
-    const data = [
-        [
-            {
-                type: "date",
-                id: "Date",
-            },
-            {
-                type: "number",
-                id: "Sum of orders",
-            },
-        ],
-        [new Date(2023, 2, 13), 37032],
-        [new Date(2023, 2, 14), 38024],
-        [new Date(2023, 2, 15), 9999999],
-        [new Date(2023, 2, 16), 38108],
-        [new Date(2023, 2, 17), 38229],
-    ];
 
+    const rawData = [{ date: new Date('2023-01-02'), sum: 1000 }, { date: new Date('2023-01-04'), sum: 1040 }, { date: new Date('2023-03-04'), sum: 10400 }]
+    const filledData = (data) => {
+        const yearsInData = data.reduce((years, chartData) => {
+            if (!years.includes(chartData.date.getUTCFullYear())) {
+                years.push(chartData.date.getUTCFullYear())
+            }
+            return years;
+        }, []).sort((a, b) => a - b)
 
-    const options = {
-        title: "Sum of orders per day",
-    };
+        const startDate = new Date(`${yearsInData[0]}`)
+        const endDate = new Date(`${yearsInData[yearsInData.length - 1] + 1}`)
+
+        const allDates = new Set();
+        for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+            allDates.add((new Date(d)).getTime());
+        }
+
+        data.forEach(chartData => {
+            allDates.delete(chartData.date.getTime())
+        })
+
+        console.log(allDates)
+        return data.concat(...[...allDates].map(date => { return { date: new Date(date), sum: 0 } }))
+    }
     return (
         <div className={styles.container}>
             <div className={styles.infoText}>Store information</div>
@@ -33,14 +35,11 @@ const Admin = () => {
                 <div className={styles.currentUsersCount}>{currnetUsersCount}</div>
             </div>
 
-            <div className={styles.chartContainer}>
-                <Chart
-                    chartType="Calendar"
-                    width="100%"
-                    height="500px"
-                    data={data}
-                    options={options}
-                />
+            <div className={styles.orderSumText}>
+                Orders sum per day:
+            </div>
+            <div className={styles.chartContainer} >
+                <Calendar data={filledData(rawData)} x={(x) => x.date} y={y => y.sum} width={1200}></Calendar>
             </div>
         </div>
     );
